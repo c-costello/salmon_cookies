a1cb88df65c0f3eba37d32f686c02d04fba841db
Clarice <claricostello@outlook.com>
2018-10-31 16:22:29 -0700

    functional footer w/o title or final total

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,10 @@
 var cookieStore = [];
 var hoursOpen = ['', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'TOTAL'];
-var finalTotal = [];
-var finalTotalArrayMiddleMan;
-var extraVar2;
+var finalTotal = 0;
+var totalArr = [];
+var hourlyArr = ['',];
+var extraVar = []; 
+
 function CookieStore(name, minCustomer, maxCustomer, avgCookies) {
   this.name = name;
   this.minCustomer = minCustomer;
@@ -19,33 +21,30 @@ function CookieStore(name, minCustomer, maxCustomer, avgCookies) {
 
 CookieStore.prototype.getCustomersPerHour = function (min, max) {
   for (var i = 0; i < this.hoursOpen.length; i++) {
-    // console.log('min', min);
-    // console.log('max',max);
-    // var randomNumber = Math.random();
-    // console.log('randomNumber', randomNumber);
-    // console.log('mathCheck1', randomNumber * (max - min + 1) + min);
-    // console.log('mathCheck2', randomNumber * (max - min + 1 ));
+    
     var randomCustPerHour = Math.floor(Math.random() * (max - min + 1) + min);
-    console.log('randomCustPerHour',randomCustPerHour);
+
     this.custPerHourArr.push(randomCustPerHour);
   }
-  // console.log('custPerHourArr', this.custPerHourArr);
+
 
 };
 
 
 CookieStore.prototype.getCookeisPerHour = function () {
   this.getCustomersPerHour(this.minCustomer, this.maxCustomer);
-  console.log('custperhour', this.custPerHourArr);
+
   for (var i = 0; i < this.hoursOpen.length; i++) {
+    while (hourlyArr.length < this.hoursOpen.length+1) {
+      hourlyArr.push(0);
+    }
     var randomCookiesPerHour = Math.floor(this.custPerHourArr[i] * this.avgCookies);
     this.cookiesPerHourArr.push(randomCookiesPerHour);
     this.dailyTotal += randomCookiesPerHour;
+    hourlyArr[i+1] += randomCookiesPerHour;
+    console.log(hourlyArr);
   }
-  console.log(this.cookiesPerHourArr);
-  finalTotalArrayMiddleMan = this.cookiesPerHourArr;
-  console.log('finalTotalArrayMiddleMan', finalTotalArrayMiddleMan);
-  finalTotal.push(finalTotalArrayMiddleMan);
+
 
 };
 
@@ -54,15 +53,15 @@ CookieStore.prototype.render = function() {
   var tbodyEl = document.getElementById('tbl-body');
   var trEl = document.createElement('tr');
   var thEl = document.createElement('th');
-
+  
   //Names of stores
   thEl.textContent = '';
   thEl.textContent = this.name;
   trEl.appendChild(thEl);
-
+  
   //For loops begin
-
-
+  
+  
   for (var i = 0; i < this.hoursOpen.length; i++) {
     var tdEl = document.createElement('td');
     tdEl.textContent = this.cookiesPerHourArr[i];
@@ -74,9 +73,10 @@ CookieStore.prototype.render = function() {
   tdTotalEl.textContent = this.dailyTotal;
   trEl.appendChild(tdTotalEl);
   tdTotalEl.id = 'tblTotal';
-
+  
   //append everything back in
   tbodyEl.appendChild(trEl);
+
 };
 
 function createTable() {
@@ -108,17 +108,12 @@ function createTable() {
     }
   }
 
+
+
   createHeader();
   // createFooter();
 }
 
-function createFooter(){
-  for (var a = 0; a < hoursOpen; a++){
-    for (var b = 0; b < cookieStore.length; b++) {
-        
-    }
-  }
-}
 
 
 
@@ -144,7 +139,25 @@ createTable();
 
 
 new CookieStore('First and Pike', 23, 65, 6.3);
-// new CookieStore('SeaTac Airport', 3, 24, 1.2);
-// new CookieStore('Seattle Center', 11, 38, 3.7);
-// new CookieStore('Capitol Hill', 20, 38, 2.3);
-// new CookieStore('Alki', 2, 16, 4.6);
+new CookieStore('SeaTac Airport', 3, 24, 1.2);
+new CookieStore('Seattle Center', 11, 38, 3.7);
+new CookieStore('Capitol Hill', 20, 38, 2.3);
+new CookieStore('Alki', 2, 16, 4.6);
+
+
+//Footer?
+function createFooter() {
+  var tfootEl = document.getElementById('tbl-foot');
+  var trforFooter = document.createElement('tr');
+  console.log('createFooterFunction is Running');
+  console.log('hourlyArr.length', hourlyArr.length);
+  for (var a = 0; a < hoursOpen.length; a++){
+    var tdforFooter = document.createElement('td');
+    tdforFooter.textContent = hourlyArr[a];
+    trforFooter.appendChild(tdforFooter);
+    console.log('tdforFooter', tdforFooter);
+    console.log('trforFooter', trforFooter);
+    tfootEl.appendChild(trforFooter);
+  }
+}
+  createFooter();
